e5818bfcaeaacf6aca8a8c51cb62d7bb100ebc4a
mohammadamin2002norouzi <114907375+mohammadamin2002norouzi@users.noreply.github.com>
2022-10-03 18:17:28 +0300

    Added comments

--- src/guessNameGame.java
+++ src/guessNameGame.java
@@ -1,39 +1,32 @@
 
 import java.util.Scanner;
+
 public class guessNameGame {
-
 	public static void main(String[] args) {
-		
+		//variable initialization
 		Scanner in = new Scanner (System.in);
-				int i=0;
-				String realname = "Amin";
-				String userName;
-				
-				do {
-					System.out.println("Guess my name (type stop to exit)");
+		int i=0;
+		String realname = "Mohammadamin";
+		String userName;
+		// loop of guessing names
+		do {
+			System.out.println("Guess my name (type stop to exit)");
 
-					userName= in.nextLine();
-					i++;
-					
-					if(userName.equals("stop")) {
-				        i--;
-						break;
-						
-					}
-					if(userName.equals("Amin")) {
-						
-					
-						System.out.println("Congratulations!");
-						break;
-						
-					}
-									
-					
-				
-	} 
-					while(!userName.equals("stop"));
-				System.out.println("You guessed "+ i + " times.");
-
-}
+			userName= in.nextLine();
+	
+			// if user stops guessing		
+			if(userName.equals("stop")) 
+				break;
+			
+			i++;
+			
+			// if user guessed right
+			if(userName.equals(realname)) {
+				System.out.println("Congratulations!");
+				break;
+			}
+		} while(!userName.equals("stop"));
+		System.out.println("You guessed "+ i + " times.");
+	}
 }
 
