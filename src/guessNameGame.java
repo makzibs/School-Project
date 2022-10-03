21ed4d29d1b253d77e234a73fd414b642e760ea3
makzibs <makzibsss@gmail.com>
2022-10-03 11:28:35 +0300

    first commit - Anil Khanal version

---
+++ src/guessNameGame.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+public class guessNameGame {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner (System.in);
+				int i=0;
+				String realname = "Emma";
+				String userName;
+				
+				do {
+					System.out.println("Guess my name (type stop to exit)");
+
+					userName= in.nextLine();
+					i++;
+					
+					if(userName.equals("stop")) {
+				        i--;
+						break;
+						
+					}
+					if(userName.equals("Emma")) {
+						
+					
+						System.out.println("Congratulations!");
+						break;
+						
+					}
+									
+					
+				
+	} 
+					while(!userName.equals("stop"));
+				System.out.println("You guessed "+ i + " times.");
+
+}
+}
+
